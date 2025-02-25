26a31039d0b0b9e80e26f3544a381ef0ab1c9bde
flexteck <flexteckse@gmail.com>
2025-02-25 15:54:24 +0100

    updated model/user.py file

--- models/user.py
+++ models/user.py
@@ -4,9 +4,11 @@ from models.base_model import BaseModel
 # A user class that inherit from BaseModel class
 
 class User(BaseModel):
+    """creation of public instance"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
     """ initialization of instance """
     super().__init__
-    self.email = ""
-    self.password = ""
-    self.first_name = ""
-    self.last_name = ""
